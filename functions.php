56781646811bfd6b999314a9d7d62832be64a781
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-09-23 18:41:01 -0400

    CSS and PHP files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+//* Do NOT include the opening php tag
+// Enqueue Ionicons.
+add_action( 'wp_enqueue_scripts', 'sp_enqueue_ionicons' );
+function sp_enqueue_ionicons() {
+	wp_enqueue_style( 'ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
+}
